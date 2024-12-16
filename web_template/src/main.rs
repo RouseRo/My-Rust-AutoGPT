d2b27b43e11320b006fb45a946563ef625e2eda2
Robert K Rouse <robert_k_rouse99@yahoo.com>
2024-12-16 12:05:15 -0800

    Update main.rs

--- web_template/src/main.rs
+++ web_template/src/main.rs
@@ -16,6 +16,25 @@ use std::fs;
 use std::io::Write;
 
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u64,
+    username: String,
+    password: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    tasks: HashMap<u64, Task>,
+    users: HashMap<u64, User>
+}
 fn main() {
     println!("Hello, world!");
 }
