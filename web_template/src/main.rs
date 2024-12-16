8f228ff1201137f3f4be387ea22c7e4cf9231c02
Robert K Rouse <robert_k_rouse99@yahoo.com>
2024-12-16 11:26:41 -0800

    Add Cargo.* and .gitignore

---
+++ web_template/src/main.rs
@@ -0,0 +1,21 @@
+
+#[allow(unused_imports)] 
+use actix_cors::Cors;
+#[allow(unused_imports)] 
+use actix_web::{http::header, web, App, HttpServer, Responder, HttpResponse};
+#[allow(unused_imports)] 
+use serde::{Deserialize, Serialize};
+
+#[allow(unused_imports)] 
+use std::sync::Mutex;
+#[allow(unused_imports)] 
+use std::collections::HashMap;
+#[allow(unused_imports)] 
+use std::fs;
+#[allow(unused_imports)] 
+use std::io::Write;
+
+
+fn main() {
+    println!("Hello, world!");
+}
