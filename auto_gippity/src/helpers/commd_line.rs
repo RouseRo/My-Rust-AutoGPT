792410424b603109c33c4a4a5d629d9794aae789
Robert K Rouse <robert_k_rouse99@yahoo.com>
2024-12-20 10:17:22 -0800

    Added commd_line.rs

---
+++ auto_gippity/src/helpers/commd_line.rs
@@ -0,0 +1,27 @@
+use crossterm::{
+    style::{Color, ResetColor, SetForegroundColor},
+    ExecutableCommand,
+};
+
+use std::io::{stdin, stdout};
+
+// Get user request
+pub fn get_user_response(question: &str) -> String {
+    let mut stdout: std::io::Stdout = stdout();
+
+    // Print the questyion in a specific color
+    stdout.execute(SetForegroundColor(Color::Blue)).unwrap();
+    println!("");
+    println!("{}", question);
+
+    // Reset Color
+    stdout.execute(ResetColor).unwrap();
+
+    // Read user input
+    let mut user_reponse: String = String::new();
+    stdin() 
+        .read_line(&mut user_reponse)
+        .expect("failed to read response");
+
+    return user_reponse.trim().to_string();
+}
