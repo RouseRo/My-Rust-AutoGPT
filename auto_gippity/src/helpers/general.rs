277c7eedec34d6104f4b5a636b12cfab3b0f3712
Robert K Rouse <robert_k_rouse99@yahoo.com>
2024-12-23 11:17:57 -0800

    Ready to start working on Basic Agents

--- auto_gippity/src/helpers/general.rs
+++ auto_gippity/src/helpers/general.rs
@@ -1,6 +1,25 @@
 use crate::apis::call_request::call_gpt;
 use crate::helpers::command_line::PrintCommand;
 use crate::models::general::llm::Message;
+use reqwest::Client;
+use serde::de::DeserializeOwned;
+use std::fs;
+
+const CODE_TEMPLATE_PATH: &str =
+    // "/Users/shaun/Code/TUTORIALS/rust_autogpt/web_template/src/code_template.rs";
+    "/Users/rober/Source/Repos/Rust/My-Rust-AutoGPT/web_template/src/code_template.rs";
+
+pub const WEB_SERVER_PROJECT_PATH: &str =
+    // "/Users/shaun/Code/TUTORIALS/rust_autogpt/web_template/";
+    "/Users/rober/Source/Repos/Rust/My-Rust-AutoGPT/web_template/";
+
+pub const EXEC_MAIN_PATH: &str =
+    // "/Users/shaun/Code/TUTORIALS/rust_autogpt/web_template/src/main.rs";
+    "/Users/rober/Source/Repos/Rust/My-Rust-AutoGPT/web_template/src/main.rs";
+
+const API_SCHEMA_PATH: &str =
+    // "/Users/shaun/Code/TUTORIALS/rust_autogpt/auto_gippity/schemas/api_schema.json";
+    "/Users/rober/Source/Repos/Rust/My-Rust-AutoGPT/schemas/api_schema.json";
 
 // Extend ai function to encourage specific output
 pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
@@ -48,6 +67,44 @@ pub async fn ai_task_request(
     }
 }
 
+// Performs call to LLM GPT - Decoded
+pub async fn ai_task_request_decoded<T: DeserializeOwned>(
+    msg_context: String,
+    agent_position: &str,
+    agent_operation: &str,
+    function_pass: for<'a> fn(&'a str) -> &'static str,
+) -> T {
+    let llm_response: String =
+        ai_task_request(msg_context, agent_position, agent_operation, function_pass).await;
+    let decoded_response: T = serde_json::from_str(llm_response.as_str())
+        .expect("Failed to decode ai response from serde_json");
+    return decoded_response;
+}
+
+// Check whether request url is valid
+pub async fn check_status_code(client: &Client, url: &str) -> Result<u16, reqwest::Error> {
+    let response: reqwest::Response = client.get(url).send().await?;
+    Ok(response.status().as_u16())
+}
+
+// Get Code Template
+pub fn read_code_template_contents() -> String {
+    let path: String = String::from(CODE_TEMPLATE_PATH);
+    fs::read_to_string(path).expect("Failed to read code template")
+}
+
+// Save New Backend Code
+pub fn save_backend_code(contents: &String) {
+    let path: String = String::from(EXEC_MAIN_PATH);
+    fs::write(path, contents).expect("Failed to write main.rs file");
+}
+
+// Save JSON API Endpoint Schema
+pub fn save_api_endpoints(api_endpoints: &String) {
+    let path: String = String::from(API_SCHEMA_PATH);
+    fs::write(path, api_endpoints).expect("Failed to write API Endpoints to file");
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -70,8 +127,9 @@ mod tests {
             "Managing Agent",
             "Defining user requirements",
             convert_user_input_to_goal,
-        ).await;
-        
+        )
+        .await;
+
         assert!(res.len() > 20);
     }
 }
